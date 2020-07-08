2ee637019761f5471a04647aca052bb5ac720fef
VanillaTank <65758613+VanillaTank@users.noreply.github.com>
2020-07-08 22:15:29 +0800

    u

---
+++ Cappadocia/js/main.js
@@ -0,0 +1,19 @@
+$(function(){
+	$('.fairy-tail__slider').slick({
+		prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="images/arrow-back.svg" alt=""></button>',
+		nextArrow: '<button type="button" class="slick-btn slick-next"><img src="images/arrow-next.svg" alt=""></button>',
+		autoplay: true,
+		autoplaySpeed: 1800,
+		fade: true
+	});
+});
+
+$(function(){
+	$('.our-trip__slider').slick({
+		prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="images/arrow-back.svg" alt=""></button>',
+		nextArrow: '<button type="button" class="slick-btn slick-next"><img src="images/arrow-next.svg" alt=""></button>',
+		autoplay: true,
+		autoplaySpeed: 1800,
+		fade: true
+	});
+});
